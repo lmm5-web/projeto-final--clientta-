57359a61601fc5eb6c8cba39fb3eadf7cf5cbc04
Letícia Macêdo Mendes <lmm5@discente.ifpe.edu.br>
2026-09-23 21:52:50 -0300

    chore: estrutura inicial do projeto Clientta

---
+++ app/src/main/java/com/example/clientta/ui/navigation/NavGraph.kt
@@ -0,0 +1,31 @@
+package com.example.clientta.ui.navigation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun NavGraph() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = NavTarget.Home.route
+    ) {
+        composable(NavTarget.Home.route) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("Clientta App")
+            }
+        }
+    }
+}
+
